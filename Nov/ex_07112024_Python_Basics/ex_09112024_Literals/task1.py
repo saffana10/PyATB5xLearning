f32b36dd9e0613aee46835717ef1768106fd08e8
Saffana10 <siddiquisaffana10@gmail.com>
2024-11-11 18:52:22 +0530

    task 9th nov

---
+++ Nov/ex_07112024_Python_Basics/ex_09112024_Literals/task1.py
@@ -0,0 +1,23 @@
+#take 3 input from the user and do operations
+#step 1 : Logic building
+#num1 , num2,num3
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+num3 = int(input("Enter num3:"))
+
+#step 2 :Rough logic
+"""sum = num1+num2
+mul = num1 * num2
+div = num1/num2
+sub = num1 - num 2 """
+
+#step 3: code
+sum = num1 + num2 + num3
+print("Sum is :",sum)
+sub = num1 - num2 - num3
+print("sub is :",sub)
+mul = num1 * num2 * num3
+print("Mul of 3 no.s is",mul)
+div = num1 / num2/num3
+print("Div of 3 no.s is:",div)
+
