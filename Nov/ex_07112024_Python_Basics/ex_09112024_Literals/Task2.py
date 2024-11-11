36ccc155c762405739562f7cbbdcb317deea3326
Saffana10 <siddiquisaffana10@gmail.com>
2024-11-11 19:01:22 +0530

    task2 9th nov

---
+++ Nov/ex_07112024_Python_Basics/ex_09112024_Literals/Task2.py
@@ -0,0 +1,13 @@
+#Take 2 input from the user and print quotient and remainder
+num1 = input("Enter number 1:\n")
+num2 = input("Enter number 2:\n")
+num1 = int(num1)
+num2 = int(num2)
+
+
+quotient = num1 // num2
+print("Quotient is :",quotient)
+
+Remainder = num1 % num2
+print("Remainder is :",Remainder)
+
